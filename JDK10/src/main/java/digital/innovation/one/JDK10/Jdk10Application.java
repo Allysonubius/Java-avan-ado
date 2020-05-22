ea3f5c82f9372fcb1c05f984903528ed9b8d3424
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-22 11:42:04 -0300

    Finalizado video aula de entendendo mais sobre Java 10.

---
+++ JDK10/src/main/java/digital/innovation/one/JDK10/Jdk10Application.java
@@ -0,0 +1,13 @@
+package digital.innovation.one.JDK10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdk10Application.class, args);
+	}
+
+}
---
+++ JDK10/src/test/java/digital/innovation/one/JDK10/Jdk10ApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.innovation.one.JDK10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
